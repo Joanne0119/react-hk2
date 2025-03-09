cbc19cd245d0f956d3c571e45a52cdc2e61ac7d2
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2025-03-10 00:18:29 +0800

    adjust changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Nav() {
   return (
     <div className='nav'>
-        <li><a href='#' className='text logo'><img src='./public/RobotLogo.png' className='logoimage'/></a></li>
+        <li><a href='#' className='text logo'><img src='./image/RobotLogo.png' className='logoimage'/></a></li>
         <li><a href='#' className='text'>Home</a></li>
         <li><a href='#' className='text'>About</a></li>
         <li><a href='#' className='text'>Contact</a></li>    
